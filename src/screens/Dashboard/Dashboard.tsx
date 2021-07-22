74a7698839c1cd6114463dea8a94df88b4a42520
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-07-22 08:35:02 -0300

    feat: adiciona styleds e reorganiza estilos

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Container, Title } from './styles';
 
 export function Dashboard() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <Title>Control Finance</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
