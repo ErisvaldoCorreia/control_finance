15f813d5de90a4e5e7a1a1b05b7d3576678e1dd2
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-07-25 12:20:21 -0300

    feat: criando a estrutura base do Card

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Container } from './styles';
+import { 
+    Container,
+    Header,
+    Title,
+    Icon,
+    Content,
+    Amount,
+    InfoTransactions,
+} from './styles';
 
 export function Cards() {
     return (
         <Container>
+            <Header>
+                <Title>Entrada</Title>
+                <Icon name='arrow-up-circle' />
+            </Header>
 
+            <Content>
+                <Amount>R$ 10.000,00</Amount>
+                <InfoTransactions>
+                    Último depósito realizado em 17 de jun!
+                </InfoTransactions>
+            </Content>
         </Container>
     );
 }
--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View``;
+
+export const Header = styled.View``;
+
+export const Title = styled.Text``;
+
+export const Icon = styled(Feather)``;
+
+export const Content = styled.View``;
+
+export const Amount = styled.Text``;
+
+export const InfoTransactions = styled.Text``;
