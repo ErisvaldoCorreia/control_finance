787913d653f57b8d0c4853f2df791fbaee928a74
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-08-14 08:31:03 -0300

    feat: cria estrutura dos elementos base

--- src/components/Transaction/Transaction.tsx
+++ src/components/Transaction/Transaction.tsx
@@ -2,10 +2,27 @@ import React from 'react';
 
 import {
     Container,
+    Title,
+    Amount,
+    InfoFooter,
+    Category,
+    Icon,
+    CategoryName,
+    DateTransaction,
 } from './styles';
 
 export function Transaction(){
     return (
-        <Container></Container>
+        <Container>
+            <Title>Desenvolvimento de Site</Title>
+            <Amount>R$ 1.200,00</Amount>
+            <InfoFooter>
+                <Category>
+                    <Icon name='dollar-signin' />
+                    <CategoryName>Vendas</CategoryName>
+                </Category>
+                <DateTransaction>10/11/2020</DateTransaction>
+            </InfoFooter>
+        </Container>
     )
 };
