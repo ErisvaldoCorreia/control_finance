bfd0ca3990afd138d879ef0815fc42d722f71332
erisvaldocorreia2 <erisvaldo.correia@rethink.dev>
2022-06-27 10:00:07 -0300

    chore: estiizacao do item

--- src/components/HistoryItem/styles.ts
+++ src/components/HistoryItem/styles.ts
@@ -6,10 +6,23 @@ interface ContainerProps {
 }
 
 export const Container = styled.View<ContainerProps>`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.shape};
+  flex-direction: row;
+  justify-content: space-between;
+  padding: ${RFValue(13)}px ${RFValue(24)}px;
+  border-radius: ${RFValue(5)}px;
+  border-left-width: ${RFValue(5)}px;
+  border-left-color: ${({ color }) => color};
+  margin-bottom: 8px;
 `;
 
 export const Amount = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(15)}px;
 `;
 
 export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(15)}px;
 `;
